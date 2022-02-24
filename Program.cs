e18047195b62e647b94e38cc2da99abec6aceb28
Weston Woodard <weston.woodard@slalom.com>
2022-02-24 16:08:55 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+// a namespace is a container for classes and functions
+namespace NumberGuesser
+{
+    // Main Class
+    internal class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            string name = "Weston Woodard";
+            int age = 30;
+
+            // START HERE //
+            // Console.WriteLine(name+ " is "+age);
+            Console.WriteLine("{0} is {1}",name,age);
+        }
+    }
+}
